983f55f5a9fc8e7320386ff841a0104b13d4d5eb
tomohiro_0225 <express22212270@gmail.com>
2024-07-25 15:53:58 +0900

    管理者アカウントの設定とユーザーが重複した時のエラー設定

--- insidesnsapp/views.py
+++ insidesnsapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User #ユーザーモデルの読み込み
+from django.db import IntegrityError #重複したときのエラー
+
 
 # Create your views here.
 def signupfunc(request):
+    if request.method == "POST":
+        username = request.POST['name']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username, '' ,password)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています。'})# {}はモデルまたは任意で出したデータ
+
+
     return render(request, 'signup.html', {})# {}はモデルまたは任意で出したデータ
