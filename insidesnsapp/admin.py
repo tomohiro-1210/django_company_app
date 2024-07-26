c9a3a62554694c3c31284e557b9b4397c8fb10ec
tomohiro_0225 <express22212270@gmail.com>
2024-07-26 10:22:46 +0900

    DBのテーブル、カラム作成（migration） 画像読み込みパッケージpillowインストール

--- insidesnsapp/admin.py
+++ insidesnsapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import snsModel
 # Register your models here.
+
+admin.site.register(snsModel)
--- insidesnsapp/models.py
+++ insidesnsapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class snsModel(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    post_img = models.ImageField(upload_to='') #upload_toは保存先を指定する
+    good = models.IntegerField() #いいね
+    read = models.IntegerField() #既読
+    readtext = models.TextField() #
+
